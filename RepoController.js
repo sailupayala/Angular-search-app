acd303dce3659f469890524c5f67e5bed7290e6c
sailakumari <sailakumari.payala@gmail.com>
2015-10-01 13:38:32 -0500

    Update RepoController.js

--- RepoController.js
+++ RepoController.js
@@ -1,6 +1,6 @@
 (function(){
       var app = angular.module("githubViewer");
-      var RepoController = function($scope, github,$routeParams) {
+      var RepoController = function($scope,$routeParams,github) {
         var onRepo=function(data){
           $scope.repo=data;
         };
@@ -10,7 +10,8 @@
         
         var reponame=$routeParams.reponame;
         var username=$routeParams.username;
-        github.getRepoDetails(username.reponame).then(onRepo,onError);
+        github.getRepoDetails(username.reponame)
+        .then(onRepo,onError);
       };
 
      app.controller("RepoController", RepoController);
